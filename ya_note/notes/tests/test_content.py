e943a08e1db0d4dfb2cf65af252ff2424de37b27
Kasyanenko Ilya <KasyanenkoID@mail.ru>
2024-08-19 14:29:25 +0500

    fix pep8

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -2,8 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from notes.models import Note
 from notes.forms import NoteForm
+from notes.models import Note
 
 User = get_user_model()
 
@@ -12,30 +12,30 @@ class TestHomePage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.OneAuthor = User.objects.create(username='OneAuthor')
-        cls.OneNote = Note.objects.create(
+        cls.one_author = User.objects.create(username='OneAuthor')
+        cls.one_note = Note.objects.create(
             title='Новая заметка',
-            author=cls.OneAuthor,
+            author=cls.one_author,
             text='Текст заметки',
             slug='slugone',
         )
 
-        cls.TwoAuthor = User.objects.create(username='TwoAuthor')
-        cls.TwoNote = Note.objects.create(
+        cls.two_author = User.objects.create(username='TwoAuthor')
+        cls.two_note = Note.objects.create(
             title='Новая заметка',
-            author=cls.TwoAuthor,
+            author=cls.two_author,
             text='Текст заметки',
             slug='slugtwo',
         )
-        cls.ThreeNote = Note.objects.create(
+        cls.three_note = Note.objects.create(
             title='Новая заметка',
-            author=cls.TwoAuthor,
+            author=cls.two_author,
             text='Текст заметки',
             slug='slugthree',
         )
 
     def test_news_count(self):
-        self.client.force_login(self.TwoAuthor)
+        self.client.force_login(self.two_author)
         url = reverse('notes:list')
         response = self.client.get(url)
         object_list = response.context['object_list']
@@ -43,18 +43,18 @@ class TestHomePage(TestCase):
         self.assertEqual(news_count, 2)
 
     def test_user_notes_do_not_include_other_users_notes(self):
-        self.client.force_login(self.TwoAuthor)
+        self.client.force_login(self.two_author)
         url = reverse('notes:list')
         response = self.client.get(url)
         object_list = response.context['object_list']
         for note in object_list:
-            self.assertEqual(note.author, self.TwoAuthor)
+            self.assertEqual(note.author, self.two_author)
 
     def test_authorized_client_has_form(self):
-        self.client.force_login(self.OneAuthor)
+        self.client.force_login(self.one_author)
         urls = (
             ('notes:add', None),
-            ('notes:edit', (self.OneNote.slug,)),
+            ('notes:edit', (self.one_note.slug,)),
         )
         for name, args in urls:
             with self.subTest(name=name, args=args):
